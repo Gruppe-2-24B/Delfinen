1fc92a94a9bcbe44d03fe9a3289f9b5c474df5d5
frederikrasmus <frederikrasmus@hotmail.dk>
2024-11-28 10:05:31 +0100

    Work3

--- src/Medlem.java
+++ src/Medlem.java
@@ -3,7 +3,7 @@ public class Medlem extends Person {
     private String aktivitetsForm;
     private boolean medlemsStatus;
 
-    public Medlem(String navn, int cprNr, int tlf, String mail, String aktivitetsForm, boolean medlemsStatus) {
+    public Medlem(String navn, int cprNr, int tlf, String mail, String aktivitetsForm) {
         super(navn, cprNr, tlf, mail);
         this.aktivitetsForm = aktivitetsForm;
         this.medlemsStatus = medlemsStatus;
@@ -12,7 +12,7 @@ public class Medlem extends Person {
         return aktivitetsForm;
     }
     public boolean getMedlemsStatus() {
-        return true;
+        return true; // Jeg er i tvivl om der skal returnes true eller false.
     }
 
 }
