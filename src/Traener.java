c655c67da1555bfc322d0013339d6c67b143df33
StudOfTheYear96 <emilroenn123@gmail.com>
2024-11-28 12:04:08 +0100

    Disciplin klasse

--- src/Traener.java
+++ src/Traener.java
@@ -1,16 +1,16 @@
 public class Traener extends Person {
     private String disciplinSpeciale;
-    private String aldersGruppe;
+    private String medlemsType;
 
-    public Traener(String navn, int cprNr, int tlf, String mail, String disciplinSpeciale, String aldersGruppe) {
+    public Traener(String navn, int cprNr, int tlf, String mail, String disciplinSpeciale, String medlemsType) {
         super(navn, cprNr, tlf, mail);
         this.disciplinSpeciale = disciplinSpeciale;
-        this.aldersGruppe = aldersGruppe;
+        this.medlemsType = medlemsType;
     }
     public String getDisciplinSpeciale() {
         return disciplinSpeciale;
     }
     public String getAldersGruppe() {
-        return aldersGruppe;
+        return medlemsType;
     }
 }
