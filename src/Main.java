2e243c004b553158422735956bbb6d4e1e502d7d
alijr8 <ahmedali.alh8@gmail.com>
2024-12-05 11:26:05 +0100

    implementeret rediger restance, betalt/ikke betalt og summen af alle kontingenter i kontingent menu

--- src/Main.java
+++ src/Main.java
@@ -14,9 +14,6 @@ import java.util.Scanner;
         Kontingent kontingent = new Kontingent();
 
 
-
-
-
         while (fortsaet) {
             System.out.println("\nHvad vil du gøre?");
             System.out.println("1. Medlemsmenu");
@@ -89,6 +86,9 @@ import java.util.Scanner;
             System.out.println("1. Vis medlemsinfo og kontingent");
             System.out.println("2. Vis medlemmer i restance");
             System.out.println("3. Beregn samlet kontingent");
+            System.out.println("4. Rediger restance-status");
+            System.out.println("5. Vis betalingsstatus for medlem");
+            System.out.println("6. Beregn summen af alle kontingenter");
             System.out.println("0. Tilbage til hovedmenu");
 
             int valg2 = input.nextInt();
@@ -107,6 +107,58 @@ import java.util.Scanner;
                 case 3:
                     ArrayList<Kontingent> alleKontingenter = new ArrayList<>();
                     kontingent.visSum(alleKontingenter);
+                    break;
+
+                case 4:
+                    System.out.println("Indtast telefonnummer for medlemmet:");
+                    int telefonnummer = input.nextInt();
+                    input.nextLine();
+
+                    Medlem medlemTilRedigering = Medlem.findMedlemVedTelefonnummer(telefonnummer);
+                    if (medlemTilRedigering != null) {
+                        System.out.println("Indtast ny restance-status (true for betalt, false for ikke betalt");
+                        boolean nyStatus = input.nextBoolean();
+                        input.nextLine();
+
+                        kontingent.setMedlem(medlemTilRedigering);
+                        kontingent.redigerRestanceStatus(nyStatus);
+                    } else {
+                        System.out.println("Medlem med telefonnummer " + telefonnummer + " blev ikke fundet");
+                    }
+                    break;
+
+                case 5:
+                    System.out.println("Indtast telefonnummer for medlemmet:");
+                    telefonnummer = input.nextInt();
+                    input.nextLine();
+
+                    Medlem medlemTilTjek = Medlem.findMedlemVedTelefonnummer(telefonnummer);
+                    if (medlemTilTjek != null) {
+                        kontingent.setMedlem(medlemTilTjek);
+                        System.out.println("Betalingsstatus for " + medlemTilTjek.getNavn() + ":");
+                        if (kontingent.erIRestance()) {
+                            System.out.println("Medlemmet er i restance");
+                        } else {
+                            System.out.println("Medlemmet har betalt");
+                        }
+                    } else {
+                        System.out.println("Medlem med telefonnummer " + telefonnummer + " blev ikke fundet");
+                    }
+                    break;
+
+                case 6:
+                    ArrayList<Kontingent> kontingentListe = new ArrayList<>();
+
+                    PersistensReader reader = new PersistensReader();
+                    reader.laesMedlemmer();
+                    for (Medlem medlem : Medlem.getAlleMedlemmer()) {
+                        Kontingent nyKontingent = new Kontingent();
+                        nyKontingent.setMedlem(medlem);
+                        kontingentListe.add(nyKontingent);
+                    }
+
+                    int samletBeloeb = Kontingent.beregnSum(kontingentListe);
+                    System.out.println("Summen af alle kontingentindbetalinger: " + samletBeloeb + " kr.");
                     break;
 
                 case 0:
